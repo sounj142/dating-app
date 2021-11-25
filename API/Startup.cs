0b3c95331bf3bd52c59dd084c63ca494d4b84d50
Hoang <sounj142@gmail.com>
2021-11-25 11:47:03 +0700

    End of session 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -51,7 +52,14 @@ namespace API
 
             app.UseHttpsRedirection();
 
+            if (env.IsDevelopment())
+            {
+                app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            }
+
             app.UseRouting();
+
+            // UseCors here ?
 
             app.UseAuthorization();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'My Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  private getUsers() {
+    this.http.get('https://localhost:44312/api/Users')
+      .subscribe(response => {
+        this.users = response;
+      }, error => {
+        console.log(error);
+      });
+  }
+}
